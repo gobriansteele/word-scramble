974aabc6eff6c55d2e76528c28b575e5c782941d
Brian Steele <brian.steele@hey.com>
2021-12-23 20:09:07 -0600

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Brian Steele on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
